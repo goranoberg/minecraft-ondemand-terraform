8dc61b48a5a10eee43df0b3a1c9106fc4dc0687e
Göran Öberg <goran.oberg@gmail.com>
2023-05-19 00:10:35 +0200

    Added default tags

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,10 +10,22 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = var.aws_region
+  default_tags {
+    tags = {
+      "Source" = "minecraft-ondemand-terraform"
+      "MineCraft" = format("minecraft.%s", var.domain_name)
+    }
+  }
 }
 
 # Configure Provider for CloudWatch since only us-east-1 is supported
 provider "aws" {
   alias  = "us-east-1"
   region = "us-east-1"
+  default_tags {
+    tags = {
+      "Source" = "minecraft-ondemand-terraform"
+      "MineCraft" = format("minecraft.%s", var.domain_name)
+    }
+  }
 }
